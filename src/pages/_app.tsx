0fb0f506f3c62f68d9b66fb96687df3a17e268f3
Usman Tayyab <usman.bestdev@gmail.com>
2023-08-16 23:57:43 +0500

    Removed module imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
 import "@/styles/globals.css";
 import "animate.css";
 import type { AppProps } from "next/app";
